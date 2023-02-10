79ca55f3f3ce1f7dac44d0e92758f275ce250496
Angstreminus <overlord12345999@gmail.com>
2023-02-10 22:19:09 +0300

    Auth and Appartment fix

--- api/models/User.go
+++ api/models/User.go
@@ -1,8 +1,18 @@
 package model
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	ID          int `gorm:"AUTO_INCREMENT"`
 	Name        string
 	Password    string
 	Appartments []Appartment
+}
+
+func Encrypt(passwd string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(passwd), bcrypt.DefaultCost)
+}
+
+func Verify(hasedPassword, passwd string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hasedPassword), []byte(passwd))
 }
