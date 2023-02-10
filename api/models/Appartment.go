5cea664dcc6d6618c6bc98d408628f409943e4bd
Angstreminus <overlord12345999@gmail.com>
2023-02-10 14:08:40 +0300

    Fixing house type appartment model field

--- api/models/Appartment.go
+++ api/models/Appartment.go
@@ -1,10 +1,44 @@
 package model
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+
+type Housing struct {
+	houseType string
+}
+
+func (h *Housing) String() string {
+	return h.houseType
+}
+
+var (
+	Undefined = Housing{""}
+	Mansion   = Housing{"Mansion"}
+	House     = Housing{"House"}
+	Room      = Housing{"Room"}
+	Hostel    = Housing{"Hostel"}
+)
+
+func GetHouseType(house string) (Housing, error) {
+	switch house {
+	case Mansion.houseType:
+		return Mansion, nil
+	case House.houseType:
+		return House, nil
+	case Room.houseType:
+		return Room, nil
+	case Hostel.houseType:
+		return Hostel, nil
+	}
+
+	return Undefined, errors.New("Unknown house type: " + house)
+}
 
 type Appartment struct {
 	ID        int
-	Type      string
+	Type      Housing
 	BookStart time.Time
 	BookEnd   time.Time
 	Service   Service
