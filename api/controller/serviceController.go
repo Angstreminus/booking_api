98bdba2f06b60c8631c06220562a9b57c1962d96
Angstreminus <overlord12345999@gmail.com>
2023-03-01 21:34:52 +0300

    Finished untested getting bill logic!

--- api/controller/serviceController.go
+++ api/controller/serviceController.go
@@ -1,6 +1,39 @@
 package controller
 
-//count days + additional service
-func GetUserSummaryBill() {
+import (
+	model "booking/api/models"
+	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+type ReqBody struct {
+	AppartmantBook_id uint
+}
+
+//count days + additional service
+func GetUserSummaryBill(ctx *gin.Context) {
+	var (
+		inputreqBody  ReqBody
+		month_numbers = 0
+	)
+	if err := ctx.ShouldBindJSON(&inputreqBody); err != nil {
+		log.Fatal(err)
+	}
+
+	app, er := model.FindAppartmentById(inputreqBody.AppartmantBook_id)
+	if er != nil {
+		log.Fatal(er)
+	}
+	months := app.BookEndDate.Sub(app.BookStartDate)
+
+	// few days or less 1 month => price for 1 month
+	month_numbers = int(months.Hours() / 24 / 30)
+	if month_numbers == 0 {
+		month_numbers++
+	}
+	finalBill := app.Service.Bill() + int32(month_numbers)*int32(app.RentPrice)
+
+	ctx.JSON(http.StatusOK, gin.H{"Your final book bill": finalBill})
 }
